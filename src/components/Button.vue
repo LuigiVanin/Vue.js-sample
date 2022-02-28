ad18ddc562a6282df93a408b6fad2e81de8ef980
LuigiVanin <luisfvaninmartins@gmail.com>
2022-02-28 12:16:02 -0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button @click="onClick()" class="btn" :style="{ background: color }">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: {
+            type: String,
+            default: "add",
+        },
+        color: {
+            type: String,
+            default: "red",
+        },
+    },
+    methods: {
+        onClick() {
+            this.$emit("toggle");
+        },
+    },
+};
+</script>
+
+<style scoped>
+button {
+    transition: 0.5s;
+    text-align: center;
+    font-weight: bold;
+    width: 150px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <h1>{{ tittle }}</h1>
+        <Button
+            @toggle="$emit('toggle')"
+            :color="showAddTask ? 'red' : 'green'"
+            :text="showAddTask ? 'Close' : 'Add Task'"
+        />
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+    name: "Header",
+    props: {
+        tittle: {
+            type: String,
+            default: "Task Tracker",
+        },
+        showAddTask: Boolean,
+    },
+    components: {
+        Button,
+    },
+};
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
